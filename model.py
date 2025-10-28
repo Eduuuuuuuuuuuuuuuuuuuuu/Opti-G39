539eb854edb84d7f61bf174b4155e725e5c04bfa
EDUARDO SOTO <eduardo.soto@uc.cl>
2025-10-28 18:19:45 -0300

    Variables actualizadas

--- model.py
+++ model.py
@@ -5,14 +5,15 @@ from converter import *
 model = Model()
 
 # Variables
-s = model.addVars(range(len(N)), range(len(T)), vtype=GRB.BINARY, name="s")
-u = model.addVars(range(len(N)), range(len(K)), range(len(T)), vtype=GRB.INTEGER, name="u")
-u_ = model.addVars(range(len(N)), range(len(K)), range(len(T)), vtype=GRB.INTEGER, name="u_")
-y = model.addVars(range(len(N)), range(len(P)), range(len(T)), vtype=GRB.BINARY, name="y")
-a = model.addVars(range(len(N)), range(len(P)), range(len(T)), vtype=GRB.CONTINUOUS, lb=0.0, ub=1.0, name="a")
-v = model.addVars(range(len(N)), range(len(T)), vtype=GRB.CONTINUOUS, lb=0.0, name="v")
-z = model.addVars(range(len(P)), range(len(T)), vtype=GRB.CONTINUOUS, lb=0.0, ub=1.0, name="z")
-o = model.addVars(range(len(N)), range(len(T)), vtype=GRB.BINARY, name="o")
+
+s = model.addVars(N, T, vtype=GRB.BINARY, name="s")
+u = model.addVars(N, K, T, vtype=GRB.INTEGER, name="u")
+u_ = model.addVars(N, K, T, vtype=GRB.INTEGER, name="u_")
+y = model.addVars(N, P, T, vtype=GRB.BINARY, name="y")
+a = model.addVars(N, P, T, vtype=GRB.CONTINUOUS, lb=0.0, ub=1.0, name="a")
+v = model.addVars(N, T, vtype=GRB.CONTINUOUS, lb=0.0, name="v")
+z = model.addVars(P, T, vtype=GRB.CONTINUOUS, lb=0.0, ub=1.0, name="z")
+o = model.addVars(N, T, vtype=GRB.BINARY, name="o")
 model.update()
 
 # Restricciones
