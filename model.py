e2d9f604cd63bb04176fbe5106cd0037a0004c97
Eduardo <eduardo.soto@uc.cl>
2025-10-28 23:53:32 -0300

    Parámetros actualizados y R1 añadida

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from gurobipy import GRB, Model
+from gurobipy import GRB, Model, quicksum
 from converter import *
 
 model = Model("EV_Charging_Chile_V2G")
@@ -18,8 +18,15 @@ model.update()
 
 # Restricciones
 
-# R1 pendiente
+# R1: Acumulación con vida útil
+for i in N:
+    for k in K:
+        for t in range(len(T)):
+            start = max(0, t-L[k]+1)
+            model.addConstr(u_[i, k, t] == quicksum(u[i, k, tau] for tau in range(len(T)) if start <= tau <= t),
+                            name="R1")
 
+# R2: Vinculación apertura / operación
 model.addConstrs(
     (o[i, t] <= s[i, t] for i in N for t in range(len(T))),
     name="R2.1")
@@ -39,5 +46,3 @@ model.addConstrs(
 model.addConstrs(
     (sum(Pot[k] * u_[i, k, t] for k in K) <= G[i][t] * s[i, t] for i in N for t in range(len(T))),
     name="R4")
-
-model.addConstrs()
